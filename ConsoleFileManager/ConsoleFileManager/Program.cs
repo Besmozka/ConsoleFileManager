85ecf877612fc0c831c39eceb897412cb3fd4dbd
SergeyAvdeev <avdey87@mail.ru>
2021-02-26 01:36:20 +0500

    DrawWindows

---
+++ ConsoleFileManager/ConsoleFileManager/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleFileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawWindows();
+            Console.ReadKey();
+        }
+
+        #region DrawWindows()
+
+        static void DrawWindows()
+        {
+            int infoWindowHeight = 5;
+            int commandLineHeight = 1;
+            Console.SetCursorPosition(0, 0);            
+            for (int i = 0; i < Console.WindowHeight; i++)
+            {
+                
+                Console.SetCursorPosition(0, i);
+                Console.Write("|");
+                if (Console.CursorTop == 0)
+                {
+                    for (int j = 1; j < Console.WindowWidth - 1; j++)
+                    {
+                        Console.SetCursorPosition(j, Console.CursorTop);
+                        Console.Write("_");
+                    }
+                }
+                if ((Console.WindowHeight - Console.CursorTop - 1) == (infoWindowHeight + commandLineHeight))
+                {
+                    for (int j = 1; j < Console.WindowWidth - 1; j++)
+                    {
+                        Console.SetCursorPosition(j, Console.CursorTop);
+                        Console.Write("_");
+                    }
+                }
+                if ((Console.WindowHeight - Console.CursorTop - 1) == commandLineHeight)
+                {
+                    for (int j = 1; j < Console.WindowWidth - 1; j++)
+                    {
+                        Console.SetCursorPosition(j, Console.CursorTop);
+                        Console.Write("_");
+                    }
+                }
+                if (Console.CursorTop == Console.WindowHeight - 1)
+                {
+                    for (int j = 1; j < Console.WindowWidth - 1; j++)
+                    {
+                        Console.SetCursorPosition(j, Console.CursorTop);
+                        Console.Write("_");
+                    }
+                }
+                Console.SetCursorPosition(Console.WindowWidth - 1, i);
+                Console.Write("|");
+            }
+            return;
+        }
+        #endregion
+
+    }
+}
